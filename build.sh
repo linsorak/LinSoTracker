795600237134c787d9c8442aa220f3ff9cb84b7e
RawZ06 <=>
2022-04-19 21:50:01 +0200

    Update builder mac script

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
 #/bin/zsh
+dmg(){
+  hdiutil create -fs HFS+ -srcfolder "$1" -volname "$2" "$2.dmg"
+}
+
 rm -rf dist
 pyinstaller --noconfirm --onefile --windowed --icon icon.ico LinSoTracker.py
+cp -r tracker.data templates dist/LinSoTracker.app/Contents/MacOS/.
 mkdir dist/LinSoTracker-install
 cd dist/LinSoTracker-install
 ln -s /Applications Applications
 cp -r ../LinSoTracker.app .
 cd ../..
+dmg ./dist/LinSoTracker-install "dist/LinSoTracker-$(arch)"
