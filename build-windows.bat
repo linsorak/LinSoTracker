d7649eb0e6e0658c86b393ded4d2befd374f6e36
LinSo <linsorak@hotmail.fr>
2026-07-16 12:47:41 +0200

    CICD and template path correction

--- build-windows.bat
+++ build-windows.bat
@@ -5,9 +5,13 @@ rmdir /s /q "_pycache_"
 rmdir /s /q "build"
 del "LinSoTracker.spec"
 
+@REM Sync properties.rc (filevers/prodvers/FileVersion/ProductVersion) to self.version
+python Tools\sync_version.py
+
 pyinstaller --clean --onefile --version-file "properties.rc" --icon "icon.ico"  "LinSoTracker.py"
 
 robocopy "templates" "dist/templates" /E
+robocopy "devtemplates" "dist/devtemplates" /E
 copy tracker.data dist\tracker.data /Y
 
 ren dist LinSoTracker-Windows
